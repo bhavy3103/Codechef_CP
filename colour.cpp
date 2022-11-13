6a34c7c7a3d34d709f6c76641ea7a12e0718171a
Bhavy Devani <105499961+bhavy3103@users.noreply.github.com>
2022-11-13 22:45:12 +0530

    Add files via upload

---
+++ colour.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int fa;
+        int a[3], ct = 0, sum = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            cin >> a[i];
+            sum = sum + a[i];
+        }
+        if (sum > 3)
+    }
+    return 0;
+}
